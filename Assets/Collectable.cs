1885a91985fd44067ad20dc9778ba8c6fc42ff1d
ALEX BEDDUS <c3035237@hallam.shu.ac.uk>
2025-03-06 13:31:30 +0000

    Clean up old file, and some whitespace

--- Assets/Collectable.cs
+++ Assets/Collectable.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(90f * Time.deltaTime, 180f * Time.deltaTime, 270f * Time.deltaTime);
